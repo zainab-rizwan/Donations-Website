eba75c1da87786f74e1c138617145d9d03f17f78
Zainab Rizwan <76821973+Zainab-Rizwan@users.noreply.github.com>
2022-06-23 13:24:36 +0500

    Add files via upload

---
+++ crud_b.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+include 'db_connection.php';
+
+
+if (isset($_POST['create'])) {
+	$title = $_POST['title'];
+	$result = mysqli_query($conn, "SELECT MAX(batch_id) as 'max' FROM batch_fund");
+	$row = mysqli_fetch_array($result);
+	$last_id= $row['max'] + 1;
+
+
+	$sql1 = "INSERT INTO `batch_fund` (`batch_id`, `batch_name`) VALUES ( '$last_id', '$title')";
+
+	if ($conn->query($sql1) === TRUE) {
+	    $_SESSION['message'] = "Batch fund created!"; 
+	    $_SESSION['msg_type'] = "Success!"; 
+	    header('location: batch_fund.php');
+	     exit();
+
+	} else {
+	    $_SESSION['message'] = $sql1 . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	    header('location: batch_fund.php');
+	    exit();
+	} 
+}
+
+
+if(isset($_POST['update']))
+{
+	$name=$_POST['name'];
+	$id=$_POST['id'];
+	$sql = "UPDATE `batch_fund` SET batch_name='$name' WHERE batch_id=$id";
+
+	if ($conn->query($sql) === TRUE) {
+	    $_SESSION['message'] = "Batch fund updated!"; 
+	    $_SESSION['msg_type'] = "Success!";
+	    header('location: batch_fund.php');
+	     exit();
+
+	} else {
+	    $_SESSION['message'] = $sql . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	   	header('location: batch_fund.php');
+
+	    exit();
+	} 
+}
+
+
+if (isset($_GET['del'])) {
+
+$id = $_GET['del'];
+$sql = "DELETE FROM `batch_fund` WHERE `batch_id`='$id'";
+if ($conn->query($sql) === TRUE) {
+	    $_SESSION['message'] = "Batch fund deleted!"; 
+	    $_SESSION['msg_type'] = "Success!"; 
+	    header('location: batch_fund.php');
+	    exit();
+
+} 
+else {
+	    $_SESSION['message'] = $sql1 . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	    header('location: batch_fund.php');
+	    exit();
+	} 
+
+} 
+
+
+	
+
+?>
---
+++ crud_p.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+include 'db_connection.php';
+
+
+if (isset($_POST['create'])) {
+	$title = $_POST['title'];
+	$result = mysqli_query($conn, "SELECT MAX(particular_id) as 'max' FROM particulars");
+	$row = mysqli_fetch_array($result);
+	$last_id= $row['max'] + 1;
+
+
+	$sql1 = "INSERT INTO `particulars` (`particular_id`, `particular_name`) VALUES ( '$last_id', '$title')";
+
+	if ($conn->query($sql1) === TRUE) {
+	    $_SESSION['message'] = "Particular created!"; 
+	    $_SESSION['msg_type'] = "Success!"; 
+	    header('location: particulars.php');
+	     exit();
+
+	} else {
+	    $_SESSION['message'] = $sql1 . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	    header('location: particulars.php');
+	    exit();
+	} 
+}
+
+
+if(isset($_POST['update']))
+{
+	$name=$_POST['name'];
+	$id=$_POST['id'];
+	$sql = "UPDATE `particulars` SET particular_name='$name' WHERE particular_id=$id";
+
+	if ($conn->query($sql) === TRUE) {
+	    $_SESSION['message'] = "Particular updated!"; 
+	    $_SESSION['msg_type'] = "Success!";
+	    header('location: particulars.php');
+	     exit();
+
+	} else {
+	    $_SESSION['message'] = $sql . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	   	header('location: particulars.php');
+
+	    exit();
+	} 
+}
+
+
+if (isset($_GET['del'])) {
+
+$id = $_GET['del'];
+$sql = "DELETE FROM `particulars` WHERE `particular_id`='$id'";
+if ($conn->query($sql) === TRUE) {
+	    $_SESSION['message'] = "Particular deleted!"; 
+	    $_SESSION['msg_type'] = "Success!"; 
+	    header('location: particulars.php');
+	    exit();
+
+} 
+else {
+	    $_SESSION['message'] = $sql1 . "<br>" . $conn->error;
+	    $_SESSION['msg_type'] = "Error";
+	    header('location: particulars.php');
+	    exit();
+	} 
+
+} 
+
+
+	
+
+?>
